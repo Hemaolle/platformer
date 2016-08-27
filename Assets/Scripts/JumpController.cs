6ecfcf366e0bb065376f63b24c5647eb7bd1a294
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2016-08-27 16:13:57 +0300

    Attempt rewriting the jumping code to fix issues

---
+++ Assets/Scripts/JumpController.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpController : MonoBehaviour {
+
+	public float groundCheckXDistance = 1f;	// How far to left and right should additional ground checks be done.
+	public Transform groundCheck;
+	public float jumpForce = 1000f;
+	public int maxForceAddTimes = 5;
+	public Animator anim;
+	public AudioSource jumpAudio;			
+
+	bool jumpButtonPressed = false;
+	bool grounded = true;
+
+	IJumpState currentJumpState;
+
+	IJumpState groundedState = new Grounded();
+	IJumpState airbornAddingForceState = new AirbornAddingForce();
+	IJumpState airbornNotAddingForceState = new AirbornAddingForce();
+
+	private void Awake()
+	{
+		currentJumpState = groundedState;
+	}
+
+	private void FixedUpdate() {
+		jumpButtonPressed = Input.GetButton("Jump");
+		grounded = DoGroundCheck();
+
+		currentJumpState.ProcessInputs(grounded, jumpButtonPressed, this);
+	}
+
+	private void AddJumpForce() {
+		GetComponent<Rigidbody2D>().AddForce (new Vector2 (0f, jumpForce));
+	}
+
+	interface IJumpState
+	{
+		void ProcessInputs(bool grounded, bool jumpButtonPressed, JumpController jumpController);
+	}
+
+	private class Grounded : IJumpState
+	{
+		public void ProcessInputs (bool grounded, bool jumpButtonPressed, JumpController jumpController)
+		{
+			if (jumpButtonPressed)
+			{
+				jumpController.anim.SetBool("Jump", true);
+				jumpController.jumpAudio.Play();
+				jumpController.AddJumpForce();
+				jumpController.currentJumpState = jumpController.airbornAddingForceState;
+			}
+		}
+	}
+
+	private class AirbornAddingForce : IJumpState
+	{
+		private int forceAddedCount;
+
+		public void ProcessInputs (bool grounded, bool jumpButtonPressed, JumpController jumpController)
+		{
+			// The player can quickly get grounded if the ceiling is low for example
+			if (grounded)
+			{
+				SetJumpControllerState(jumpController, jumpController.groundedState);
+			}
+			else if (jumpButtonPressed) {				
+				forceAddedCount++;
+				if (forceAddedCount > jumpController.maxForceAddTimes)
+				{
+					SetJumpControllerState(jumpController, jumpController.airbornNotAddingForceState);
+				}
+				jumpController.AddJumpForce();
+			}
+
+			// jump button not pressed
+			else {
+				SetJumpControllerState(jumpController, jumpController.airbornNotAddingForceState);
+			}
+		}
+
+		private void SetJumpControllerState(JumpController jumpController, IJumpState newState)
+		{
+			forceAddedCount = 0;
+			jumpController.currentJumpState = newState;
+		}
+	}
+
+	private class AirbornNotAddingForce : IJumpState {
+		
+		public void ProcessInputs (bool grounded, bool jumpButtonPressed, JumpController jumpController)
+		{
+			if (grounded) {
+				jumpController.currentJumpState = jumpController.groundedState;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Checks if the player stands on the ground.
+	/// </summary>
+	private bool DoGroundCheck ()
+	{
+		Vector3 groundCheckMiddleStart = transform.position;
+		Vector3 groundCheckLeftStart = transform.position - Vector3.right * groundCheckXDistance;
+		Vector3 groundCheckRightStart = transform.position + Vector3.right * groundCheckXDistance;
+		Vector3 groundCheckMiddleEnd = groundCheck.position;
+		Vector3 groundCheckLeftEnd = groundCheck.position - (Vector3.right * groundCheckXDistance);
+		Vector3 groundCheckRightEnd = groundCheck.position + (Vector3.right * groundCheckXDistance);
+
+		// The player is grounded if three linecasts from player position to the groundcheck position hits anything on
+		// the ground layer.
+		bool grounded = Physics2D.Linecast (groundCheckMiddleStart, groundCheckMiddleEnd, 
+			1 << LayerMask.NameToLayer ("Ground")) 
+			|| Physics2D.Linecast (groundCheckLeftStart, groundCheckLeftEnd, 1 << LayerMask.NameToLayer ("Ground")) 
+			|| Physics2D.Linecast (groundCheckRightStart, groundCheckRightEnd, 1 << LayerMask.NameToLayer ("Ground"));
+		Debug.DrawRay (groundCheckMiddleStart, groundCheckMiddleEnd - groundCheckMiddleStart);
+		Debug.DrawRay (groundCheckLeftStart, groundCheckLeftEnd - groundCheckLeftStart);
+		Debug.DrawRay (groundCheckRightStart, groundCheckRightEnd - groundCheckRightStart);
+
+		return grounded;
+	}
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,63 +9,36 @@ public class PlayerControl : MonoBehaviour
 	[HideInInspector]
 	public bool facingRight = true;			// For determining which way the player is currently facing.
 	[HideInInspector]
-	public bool jump = false;				// Condition for whether the player should jump.
-	[HideInInspector]
 	public bool attack = false;				// Condition for whether the player should attack.
 	
 	public GameObject punchObject;
 	
 	public float moveForce = 365f;			// Amount of force added to move the player left and right.
 	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
-	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
-	public int jumpForceTimes = 5;			// Maximum amount of FixedUpdates in which the jump force will be applied.
-											// Jump force can be applied multiple times during the same jump to achieve
-											// jumping higher when the jump button is pressed for a longer time.
+
 	public float projectileSpeed = 5f;		// Speed of the the projectile of the projectile attack.
 	public float groundCheckXDistance = 1f;	// How far to left and right should additional ground checks be done.
 	public float walkAudioInterval = 0.3f;	// How often should the sound of footsteps be played.
 
-	public AudioSource jumpAudio;			
 	public AudioSource walkAudio;			
 	public AudioSource shootAudio;
 
 	private Transform groundCheck;			// A position marking where to check if the player is grounded.
 	private bool grounded = false;			// Whether or not the player is grounded.
 	private Animator anim;					// Reference to the player's animator component.	
-	private bool jumpButtonReleased = true;
-	
-	
+
 	private float lastWalkSound;
-	private float jumpStartTime;
-	private int jumpForceCount = 0;
 	
 	void Awake()
 	{
 		// Setting up references.
-		groundCheck = transform.Find("groundCheck");
 		anim = GetComponent<Animator>();
 	}
 	
 	
 	void Update()
 	{
-		DoGroundCheck ();
-
-		// If the jump button is pressed and the player is grounded then the player should jump.
-		if (Input.GetButton ("Jump")) {
-			jump = true;
-			// If the player stands on the ground and the jump button was just pressed start a new jump.
-			if(grounded && jumpButtonReleased)
-			{
-				jumpForceCount = 0;
-				jumpAudio.Play();
-				jumpButtonReleased = false;
-			}
-		}
-		
-		if (Input.GetButtonUp("Jump"))
-			jumpButtonReleased = true;
-		
+		DoGroundCheck();
 		if(Input.GetButtonDown("Fire1"))
 			attack = true;		
 	}	
@@ -73,7 +46,6 @@ public class PlayerControl : MonoBehaviour
 	void FixedUpdate ()
 	{
 		HandleWalking();		
-		HandleJumping();	
 		HandleAttacking();	
 	}
 
@@ -115,7 +87,7 @@ public class PlayerControl : MonoBehaviour
 			Flip();
 
 		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		if (Mathf.Abs (rigidbody2D.velocity.x) > 0.1f) {
+		if (Mathf.Abs (GetComponent<Rigidbody2D>().velocity.x) > 0.1f) {
 			anim.SetBool ("Running", true);
 			if (grounded && Time.timeSinceLevelLoad - lastWalkSound > walkAudioInterval) {
 				walkAudio.Play ();
@@ -126,30 +98,7 @@ public class PlayerControl : MonoBehaviour
 			anim.SetBool ("Running", false);
 		}
 
-		rigidbody2D.velocity = new Vector2(horizontalInput * maxSpeed, rigidbody2D.velocity.y);
-	}
-
-	void HandleJumping ()
-	{
-		if (grounded)
-		{
-			anim.SetBool ("Jump", false);
-		}
-
-		// If the jump button is released during a jump, no more jump forces can be applied.
-		if (jumpForceCount > 0 && jump == false)
-			jumpForceCount = jumpForceTimes;
-
-		// Check if jump button is pressed and maximum number of jump forces hasn't been applied
-		if (jump && (jumpForceCount < jumpForceTimes)) {
-			// Set the Jump animator trigger parameter.
-			if (!anim.GetBool ("Jump"))
-				anim.SetBool ("Jump", true);
-			// Add a vertical force to the player.
-			rigidbody2D.AddForce (new Vector2 (0f, jumpForce));
-			jump = false;
-			jumpForceCount++;
-		}
+		GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalInput * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 	}
 	
 	void HandleAttacking() {
@@ -164,7 +113,6 @@ public class PlayerControl : MonoBehaviour
 			shootAudio.Play();
 		}
 	}
-	
 	
 	void Flip ()
 	{
